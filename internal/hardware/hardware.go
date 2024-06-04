44b39a2ab386e146c8804b6a0ae7d73ea90aa259
Manuel Firman <manuel.firman@gmail.com>
2024-06-04 10:25:41 -0300

    feat: hardware monitor implementation added + changes in entities structs

--- internal/hardware/hardware.go
+++ internal/hardware/hardware.go
@@ -1,23 +1,35 @@
 package hardware
 
+import "errors"
+
+var (
+	// ErrNoCPUInfo is the error for no CPU info.
+	ErrNoCPUInfo = errors.New("no cpu info")
+)
+
 // SystemSection is the structure for the system section.
 type SystemSection struct {
-	RuntimeOS     string
-	VirtualMemory uint64
-	Host          string
+	RuntimeOS string
+	TotalVM   uint64
+	UsedVM    uint64
+	Host      string
 }
 
 // CPUSection is the structure for the CPU section.
 type CPUSection struct {
 	ModelName string
-	Cores     int
+	Family    string
+	Speed     float64
+	FirstCPU  []float64
+	SecondCPU []float64
 }
 
 // DiskSection is the structure for the disk section.
 type DiskSection struct {
-	TotalSpace uint64
-	FreeSpace  uint64
-	UsedSpace  uint64
+	TotalSpace     uint64
+	FreeSpace      uint64
+	UsedSpace      uint64
+	PercentageUsed float64
 }
 
 // HardwareMonitor is the interface that wraps the basic methods of a hardware monitor.
--- internal/hardware/hardware_default.go
+++ internal/hardware/hardware_default.go
@@ -1,7 +1,84 @@
 package hardware
 
+import (
+	"runtime"
+
+	"github.com/shirou/gopsutil/v4/cpu"
+	"github.com/shirou/gopsutil/v4/disk"
+	"github.com/shirou/gopsutil/v4/host"
+	"github.com/shirou/gopsutil/v4/mem"
+)
+
 // megaByteDiv is the division of a megabyte.
 const megabyteDiv uint64 = 1024 * 1024
 
 // gigabyteDiv is the division of a gigabyte.
 const gigabyteDiv uint64 = megabyteDiv * 1024
+
+// SystemSection is the structure for the system section.
+func GetSystemSection() (s SystemSection, err error) {
+	// Get the runtime OS.
+	s.RuntimeOS = runtime.GOOS
+
+	// Get the virtual memory stats.
+	vmStats, err := mem.VirtualMemory()
+	if err != nil {
+		return
+	}
+	s.TotalVM = vmStats.Total / megabyteDiv
+	s.UsedVM = vmStats.Used / megabyteDiv
+
+	// Get the host info.
+	hostInfo, err := host.Info()
+	if err != nil {
+		return
+	}
+	s.Host = hostInfo.Hostname
+
+	return
+}
+
+// GetCPUSection returns the CPU section.
+func GetCPUSection() (c CPUSection, err error) {
+	// Get the CPU info.
+	cpuStat, err := cpu.Info()
+	if err != nil {
+		return
+	}
+	if len(cpuStat) == 0 {
+		err = ErrNoCPUInfo
+		return
+	}
+	// Set the CPU info if there is any.
+	c.ModelName = cpuStat[0].ModelName
+	c.Family = cpuStat[0].Family
+	c.Speed = float64(cpuStat[0].Mhz)
+
+	// Get the CPU percentage.
+	percentage, err := cpu.Percent(0, true)
+	if err != nil {
+		return
+	}
+	// Set the CPU percentage.
+	c.FirstCPU = percentage[:len(percentage)/2]
+	c.SecondCPU = percentage[len(percentage)/2:]
+
+	return
+}
+
+// GetDiskSection returns the disk section.
+func GetDiskSection() (d DiskSection, err error) {
+	// Get the disk stats.
+	diskStat, err := disk.Usage("/")
+	if err != nil {
+		return
+	}
+
+	// Set the disk stats.
+	d.TotalSpace = diskStat.Total / gigabyteDiv
+	d.FreeSpace = diskStat.Free / gigabyteDiv
+	d.UsedSpace = diskStat.Used / gigabyteDiv
+	d.PercentageUsed = diskStat.UsedPercent
+
+	return
+}
