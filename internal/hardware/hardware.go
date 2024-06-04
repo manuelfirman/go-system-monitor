c4d32323ef9243b168b9944fc9f2a6786dcf1af2
Manuel Firman <manuel.firman@gmail.com>
2024-06-04 10:01:40 -0300

    feat: hardware interface added + entities added

--- internal/hardware/hardware.go
+++ internal/hardware/hardware.go
@@ -1,1 +1,31 @@
 package hardware
+
+// SystemSection is the structure for the system section.
+type SystemSection struct {
+	RuntimeOS     string
+	VirtualMemory uint64
+	Host          string
+}
+
+// CPUSection is the structure for the CPU section.
+type CPUSection struct {
+	ModelName string
+	Cores     int
+}
+
+// DiskSection is the structure for the disk section.
+type DiskSection struct {
+	TotalSpace uint64
+	FreeSpace  uint64
+	UsedSpace  uint64
+}
+
+// HardwareMonitor is the interface that wraps the basic methods of a hardware monitor.
+type HardwareMonitor interface {
+	// GetSystemSection returns the system section.
+	GetSystemSection() (SystemSection, error)
+	// GetCPUSection returns the CPU section.
+	GetCPUSection() (CPUSection, error)
+	// GetDiskSection returns the disk section.
+	GetDiskSection() (DiskSection, error)
+}
---
+++ internal/hardware/hardware_default.go
@@ -0,0 +1,7 @@
+package hardware
+
+// megaByteDiv is the division of a megabyte.
+const megabyteDiv uint64 = 1024 * 1024
+
+// gigabyteDiv is the division of a gigabyte.
+const gigabyteDiv uint64 = megabyteDiv * 1024
